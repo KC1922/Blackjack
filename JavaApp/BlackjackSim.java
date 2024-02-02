df9505c9f1ffb2bd61cf06266ccf79aaf208de7c
Kyle Cwik <kylecwik@gmail.com>
2024-02-01 20:14:01 -0500

    refactor: gameplay logic overhauled
    more functions added to split game play logic into reasonable parts. Main now only handles the entry, gameLoop handles the prompting of the user each round, and player/dealer turns are now their own functions.

--- JavaApp/BlackjackSim.java
+++ JavaApp/BlackjackSim.java
@@ -1,155 +1,208 @@
+import java.util.Scanner;
 
 public class BlackjackSim {
 
+    //static variables
     static int playerMoney = 1000;
-    public static void main(String[] args) {
-        //create shoe with 4 decks
-        Shoe shoe = new Shoe(4);
+    static Shoe shoe = new Shoe(4);
+    static Hand playerHand = new Hand();
+    static Hand dealerHand = new Hand();
 
-        
+    static String Player = "Player";
+    static String Dealer = "Dealer";
+
+    public static void main(String[] args) {
         int playerBet = 0;
+        shoe.shuffle();
 
         System.out.println("\nWelcome to Blackjack! Type 'quit' to end the game or go bankrupt!");
+        System.out.println("You have $" + playerMoney + ". How much would you like to bet?");
 
-        while (playerMoney > 0) {
-            Boolean playerBust = false;
-            Boolean playerBlackjack = false;
+        playerBet = getBet();
 
-            System.out.println("\n-------------------------");
-            System.out.println("\tNew Round");
-            System.out.println("-------------------------");
-
-
-            System.out.println("You have $" + playerMoney + ". How much would you like to bet?");
-            String input = System.console().readLine();
-            if(input.equalsIgnoreCase("quit")) {
+        while (playerMoney > 0){
+            String roundResult = gameLoop(playerBet);
+            if (roundResult.equals("quit")) {
                 break;
             }
+            if (playerMoney <= 0) {
+                System.out.println("\nYou're out of money! Better luck next time!");
+                break;
+            }
+            System.out.println("\nYou have $" + playerMoney + ". How much would you like to bet?");
+            playerBet = 0;
+            playerBet = getBet();
+            
+        }
+        
+        System.out.println("\nGame ended! You ended with $" + playerMoney);
+        System.out.println("\nThanks for playing!");
+
+    }
+    //function handles the initial deal of the game
+    public static void initialDeal() {
+        playerHand.addCard(shoe.dealCard());
+        playerHand.addCard(shoe.dealCard());
+        dealerHand.addCard(shoe.dealCard());   
+    }
+
+    //function deals with basic game loop of blackjack, calls playerTurn and dealerTurn
+    public static String gameLoop(int playerBet){
+        //ensure hands are empty before start of round
+        if (!playerHand.getCards().isEmpty() || !dealerHand.getCards().isEmpty()) {
+            playerHand.clearHand();
+            dealerHand.clearHand();
+        }
+
+        System.out.println("\n-------------------------\n\tRound Start\n-------------------------");
+
+        //deal initial cards and calculate hand values
+        initialDeal();
+        playerHand.calcPlayerHandValue();
+        dealerHand.calcDealerHandValue();
+
+        playerHand.printHand(Player);
+        delay(1000);
+        dealerHand.printHand(Dealer);
+        delay(1000);
+
+        //check for natural blackjack
+        if (playerHand.isBlackjack()) {
+            System.out.println("\nNatural Blackjack! You win!");
+            return "blackjack";
+        }
+
+        //player turn
+        String playerResult = playerTurn(playerBet);
+        if (playerResult.equals("quit")) {
+            return "quit";
+        } else if (playerResult.equals("bust")) {
+            System.out.println("\nYou busted! You lose!");
+            playerMoney -= playerBet;
+            return "bust";
+        } else if (playerResult.equals("blackjack")) {
+            System.out.println("\nBlackjack! You win!");
+            playerMoney += playerBet*2;
+            return "blackjack";
+        }
+
+        dealerTurn();
+
+        //determine game end scenario
+        determineGameEndScenario(playerHand.getHandValue(), dealerHand.getHandValue(), playerBet);
+
+        System.out.println("\n-------------------------\n\tRound End\n-------------------------");
+
+        return "roundEnd";
+    }
+
+    public static String playerTurn(int playerBet){
+        String playerAction = "";
+    
+        System.out.println("\n-------------------------\n\tPlayer's Turn\n-------------------------");
+    
+        Scanner scanner = new Scanner(System.in);
+    
+        while (!playerAction.equalsIgnoreCase("stand")) {
+            System.out.println("\nWhat would you like to do? (hit/stand)");
+            playerAction = scanner.nextLine();
+    
+            //input validation
+            if (playerAction.equalsIgnoreCase("quit")) {
+                return "quit";
+            }
+            while (!playerAction.equalsIgnoreCase("hit") && !playerAction.equalsIgnoreCase("stand")) {
+                System.out.println("Invalid input! Please enter 'hit' or 'stand'.");
+                playerAction = scanner.nextLine();
+            }
+    
+            if (playerAction.equalsIgnoreCase("hit")) {
+                playerHand.addCard(shoe.dealCard());
+                playerHand.calcPlayerHandValue();
+    
+                //print hand
+                playerHand.printHand(Player);
+                delay(1000);
+    
+                //check for bust
+                if (playerHand.getHandValue() > 21) {
+                    return "bust";
+                }
+                //check for blackjack
+                if (playerHand.isBlackjack()) {   
+                    return "blackjack";
+                }
+            }
+        }
+    
+        return "endPlayerTurn";
+    }
+    
+    public static void dealerTurn(){
+        System.out.println("\n-------------------------\n\tDealer's Turn\n-------------------------");
+    
+        //print dealer's hand
+        dealerHand.printHand(Dealer);
+        delay(1000);
+    
+        //dealer hits until hand value is 17 or greater
+        while (dealerHand.getHandValue() < 17) {
+            dealerHand.addCard(shoe.dealCard());
+            dealerHand.calcDealerHandValue();  
+            delay(500);
+            System.out.println("\nDealer hits!");
+        }
+
+        //print dealer's hand
+        dealerHand.printHand(Dealer);
+        delay(1000);
+        return;
+    }
+ 
+    public static void determineGameEndScenario(int playerHandValue, int dealerHandValue, int playerBet) {
+        if (dealerHandValue > 21) {
+            System.out.println("\nDealer busted! You win!");
+            playerMoney += playerBet*2;
+        } else if (dealerHandValue == 21) {
+            System.out.println("\nDealer has blackjack! You lose!");
+            playerMoney -= playerBet;
+        } else if (dealerHandValue > playerHandValue) {
+            System.out.println("\nDealer wins with the greater hand!");
+            playerMoney -= playerBet;
+        } else if (dealerHandValue < playerHandValue) {
+            System.out.println("\nYou win with the greater hand!");
+            playerMoney += playerBet*2;
+        } else {
+            System.out.println("\nPush! Bet returned!");
+        }
+    }
+
+    public static int getBet(){
+        int bet = 0;
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();   
+            if (input.equalsIgnoreCase("quit")) {
+                System.out.println("Thanks for playing!");
+                System.exit(0);
+            }
             try {
-                playerBet = Integer.parseInt(input);
+                bet = Integer.parseInt(input);
             } catch (NumberFormatException e) {
                 System.out.println("Invalid input! Please enter a number.");
                 continue;
             }
-            if (playerBet > playerMoney) {
+            if (bet > playerMoney) {
                 System.out.println("You don't have that much money!");
                 continue;
             }
-            if (playerBet <= 0) {
+            if (bet <= 0) {
                 System.out.println("You must bet at least $1!");
                 continue;
             }
-
-            //create initial hands
-            Hand playerHand = new Hand();
-            playerHand.addCard(shoe.dealCard());
-            playerHand.addCard(shoe.dealCard());
-            playerHand.calcPlayerHandValue();
-
-            if (playerHand.isBlackjack()) {
-                System.out.println("Natural Blackjack! You win!");
-                playerMoney += playerBet*2;
-                continue;
-            }
-
-            Hand dealerHand = new Hand();
-            dealerHand.addCard(shoe.dealCard());
-            dealerHand.calcDealerHandValue();
-            
-            System.out.println("Your hand: " + String.join(", ", playerHand.getCards()) + "\nWith a value of: " + playerHand.getHandValue());
-            
-            delay(2000);
-
-            System.out.println("Dealer's hand: " + dealerHand.getCards().get(0) + "\nWith a value of: " + dealerHand.getHandValue());
-            
-            delay(2000);
-
-            System.out.println("\n-------------------------");
-            System.out.println("\tYour Turn");
-            System.out.println("-------------------------");
-            
-            String playerAction = "";
-            System.out.println("Would you like to hit or stand?");
-            playerAction = System.console().readLine();
-            //input validation
-            if (playerAction.equalsIgnoreCase("quit")) {
-                break;
-            }
-            while (!playerAction.equalsIgnoreCase("hit") && !playerAction.equalsIgnoreCase("stand")) {
-                System.out.println("Invalid input! Please enter 'hit' or 'stand'.");
-                playerAction = System.console().readLine();
-            }
-
-            //Player's turn
-            while (playerAction.equalsIgnoreCase("hit")) {
-                delay(1000);
-                playerHand.addCard(shoe.dealCard());
-                playerHand.calcPlayerHandValue();
-                //playerHandValue = playerHand.getPlayerHandValue();
-                System.out.println("Your hand: " + String.join(", ", playerHand.getCards()) + "\nWith a value of: " + playerHand.getHandValue());
-                if (playerHand.isBust()) {
-                    System.out.println("You busted!");
-                    playerBust = true;
-                    playerMoney -= playerBet;
-                    break;
-                }
-                if (playerHand.isBlackjack()) {
-                    System.out.println("Blackjack! You win!");
-                    playerMoney += playerBet*2;
-                    playerBlackjack = true;
-                    break;
-                }
-                System.out.println("Would you like to hit or stand?");
-                playerAction = System.console().readLine();
-                if (playerAction.equalsIgnoreCase("quit")) {
-                    break;
-                }
-                while (!playerAction.equalsIgnoreCase("hit") && !playerAction.equalsIgnoreCase("stand")) {
-                    System.out.println("Invalid input! Please enter 'hit' or 'stand'.");
-                    playerAction = System.console().readLine();
-                }
-            }
-  
-            //Dealer's turn
-            if (!playerBust && !playerBlackjack) {
-                System.out.println("\n-------------------------");
-                System.out.println("\tDealer's Turn");
-                System.out.println("-------------------------");
-                //it is common practice for the dealer to hit until they have a hand value of 17 or greater
-                //so that is simulated here
-                while (dealerHand.getHandValue() < 17) {
-                    dealerHand.addCard(shoe.dealCard());
-                    dealerHand.calcDealerHandValue();
-                }
-                System.out.println("Dealer's hand: " + String.join(", ", dealerHand.getCards()) + "\nWith a value of: " + dealerHand.getHandValue());
-                
-                delay(2000);
-                
-                //call determineWinner to determine who won the round
-                determineWinner(playerHand.getHandValue(), dealerHand.getHandValue(), playerBet);
-            }
-
+            break;
         }
-        System.out.println("\nGame over! You ended with $" + playerMoney);
-        System.out.println("\nThanks for playing!");
-    }
-    
-    public static void determineWinner(int playerHandValue, int dealerHandValue, int playerBet) {
-        if (dealerHandValue > 21) {
-            System.out.println("Dealer busted! You win!");
-            playerMoney += playerBet*2;
-        } else if (dealerHandValue == 21) {
-            System.out.println("Dealer has blackjack! You lose!");
-            playerMoney -= playerBet;
-        } else if (dealerHandValue > playerHandValue) {
-            System.out.println("Dealer wins with the greater hand!");
-            playerMoney -= playerBet;
-        } else if (dealerHandValue < playerHandValue) {
-            System.out.println("You win with the greater hand!");
-            playerMoney += playerBet*2;
-        } else {
-            System.out.println("Push! Bet returned!");
-        }
+        return bet;
     }
 
     public static void delay(int ms) {
